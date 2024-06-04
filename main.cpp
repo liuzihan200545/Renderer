2aa7bb664ffaec3f0b57e8bcfedd74eb08eb63cd
liuzihan200545 <3069522719@qq.com>
2024-06-04 18:15:22 +0800

    项目初始化

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 
 SDL_Window* window;
 SDL_Renderer* renderer;
+bool is_running = false;
 
 bool initialize_window(){
     if(SDL_Init(SDL_INIT_EVERYTHING)!=0){
@@ -31,9 +32,47 @@ bool initialize_window(){
     return true;
 }
 
+void setup();
+void process_input();
+void update();
+void render();
 
 int main(){
-    bool is_running = initialize_window();
-    std::cout<<is_running<<std::endl;
+    is_running = initialize_window();
+    setup();
+    while(is_running){
+        process_input();
+        update();
+        render();
+    }
     return 0;
+}
+
+void setup(){
+
+}
+
+void process_input(){
+    SDL_Event event;
+    SDL_PollEvent(&event);
+    switch (event.type) {
+        case SDL_QUIT:
+            is_running = false;
+            break;
+        case SDL_KEYDOWN:
+            if(event.key.keysym.sym == SDLK_ESCAPE){
+                is_running = false;
+            }
+            break;
+    }
+}
+
+void update(){
+
+}
+
+void render(){
+    SDL_SetRenderDrawColor(renderer,255,0,0,255);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
 }
